d07afda4db3ed768a85144db204003d68ed3494a
Jagraj Singh <singhjagraj900@gmail.com>
2020-04-28 00:15:02 +0100

    v2
    Database functionality and being able to complete tasks.

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    complete = db.Column(db.Boolean)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    incomplete = ToDo.query.filter_by(complete=False).all()
+    complete = ToDo.query.filter_by(complete=True).all()
+    return render_template('home.html', incomplete=incomplete, complete=complete)
+
+@app.route('/add', methods=['POST'])
+def add():
+    todo = ToDo(text=request.form['todoitem'], complete=False)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/complete/<id>')
+def complete(id):
+    todo = ToDo.query.filter_by(id=int(id)).first()
+    todo.complete = True
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
